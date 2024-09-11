3d46a2406a6f31f0b485f9865e19793a7e2d6b1e
DMegatron <soujatyabhunia2003@gmail.com>
2024-09-11 12:54:08 +0530

    Made the input fields change color when invalid input is entered.

--- script.js
+++ script.js
@@ -1,72 +1,76 @@
 var email;
 var password;
 var errorMessages = [];
+var flag = 0;
+var flag1 = 0;
 
 function validate(event) {
+    event.preventDefault();
     email = document.querySelector('.email');
     password = document.querySelector('.password');
     errorMessages = [];
+    flag = 0;
+    flag1 = 0;
 
-
-    // removeErrorMessages(email);
-    // removeErrorMessages(password);
 
     if (!(email.value.includes('@') && email.value.includes('.'))) {
         errorMessages.push("Email is not valid.");
+        email.style.backgroundColor = "rgba(255, 121, 121, 0.8)";
         email.focus();
+        flag = 1;
     }
 
     if (password.value.length < 8) {
         errorMessages.push("Password should be at least 8 characters long.");
+        flag1 = 1;
     }
 
     if (!/[A-Z]/.test(password.value)) {
         errorMessages.push("Password should contain at least one uppercase letter.");
+        flag1 = 1;
     }
 
     if (!/[a-z]/.test(password.value)) {
         errorMessages.push("Password should contain at least one lowercase letter.");
+        flag1 = 1;
     }
 
     if (!/[0-9]/.test(password.value)) {
         errorMessages.push("Password should contain at least one digit.");
+        flag1 = 1;
     }
 
     if (!/[^a-zA-Z0-9]/.test(password.value)) {
         errorMessages.push("Password should contain at least one special character.");
+        flag1 = 1;
     }
 
     if (errorMessages.length > 0) {
-        password.focus();
+
+        if(flag1){
+            showErrorField(password);
+        }
+
+        if(!flag){
+            password.focus();
+        }
         showError(errorMessages.join('\n'));
         errorMessages = [];
         return false;
     }
 
-    return true;
+    if(!(flag || flag1)){
+        return true;
+    }
 
 }
 
 function showError(errorMessage) {
     alert(errorMessage);
-    // input.style.borderWidth = "2px";
-    // input.style.borderColor = "red";
-    // input.style.borderStyle = "solid";
-    
-    // var errorMsg = document.createElement('div');
-    // errorMsg.innerText = errorMessage;
-    // errorMsg.classList.add('error-message');
-    // errorMsg.style.color = "red";
-    // errorMsg.style.fontFamily = "Arial";
-    // errorMsg.style.width = "81%";
-    // errorMsg.style.marginTop = "5px";
-    // errorMsg.style.textAlign = "left";
-    // input.insertAdjacentElement('afterend', errorMsg);
-    // input.focus();
 }
 
-// function removeErrorMessages(input) {
-//     if (input.nextElementSibling && input.nextElementSibling.classList.contains('error-message')) {
-//         input.nextElementSibling.remove();
-//     }
-// }
+
+
+function showErrorField(element){
+    element.style.backgroundColor = "rgba(255, 121, 121, 0.8)";
+}
